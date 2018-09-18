97ce9e893f53ece9b138f6b3af7859443ccc62e0
santhosh921 <43337965+santhosh921@users.noreply.github.com>
2018-09-18 12:39:24 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\nInput:)
+scanf("%d",&n);
+if(n>0)
+{
+  if(n%2==0)
+  printf("\nEven");
+  else
+  printf("\nOdd");
+}
+else
+printf("\ninvalid");
+getch();
+}
+  
